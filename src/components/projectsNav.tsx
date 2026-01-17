cb446d7fa3e8a27598c35eccd19b38e4b492d9b0
lucasfgaldinos <lucasfgaldinos@outlook.com>
2026-01-17 19:39:24 -0300

    style: Adjusting the style of sectionTitle and projectsNav.

--- src/components/projectsNav.tsx
+++ src/components/projectsNav.tsx
@@ -12,13 +12,13 @@ export function ProjectsNav() {
   const [isActive, setIsActive] = useState<OptionsProps>("Destaques");
 
   return (
-    <nav className="flex flex-row gap-1.5 bg-gray-100 w-fit rounded-full p-1.5 mt-5">
+    <nav className="flex flex-row gap-1.5 bg-gray-100 w-fit rounded-full p-1.5 mx-auto md:mx-0">
       {options.map((item) => (
         <button
           onClick={() => setIsActive(item)}
           key={item}
           className={`
-              py-2 px-4 flex items-center justify-center rounded-full cursor-pointer font-medium transition
+              py-1 px-2 md:py-2 md:px-4 flex items-center justify-center rounded-full text-xs md:text-base cursor-pointer font-medium transition
               ${isActive === item ? "hover:white" : "hover:bg-gray-200"}
               ${isActive === item && "bg-white shadow"}
               `}
