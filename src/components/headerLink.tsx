608f30e8ed8cef4087f5904f8d73fe1de57fc283
lucasfgaldinos <lucasfgaldinos@outlook.com>
2026-01-29 17:21:44 -0300

    style: Adds transition only on hover of the headerLink.

--- src/components/headerLink.tsx
+++ src/components/headerLink.tsx
@@ -4,7 +4,7 @@ export function HeaderLink({ children, ...props }: HeaderLinkProps) {
   return (
     <div
       {...props}
-      className="select-none px-3 py-1 rounded-bl-2xl rounded-br-sm rounded-tl-sm rounded-tr-2xl transition-colors backface-hidden group text-my-primary-text font-medium cursor-pointer flex items-center gap-1 hover:bg-my-text-blue"
+      className="select-none px-3 py-1 rounded-bl-2xl rounded-br-sm rounded-tl-sm rounded-tr-2xl hover:transition-colors backface-hidden group text-my-primary-text font-medium cursor-pointer flex items-center gap-1 hover:bg-my-text-blue"
     >
       {children}
     </div>
