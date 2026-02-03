930690862e50aab359c54c64e6800e0391ca5fc8
lucasfgaldinos <lucasfgaldinos@outlook.com>
2026-02-03 17:19:09 -0300

    style: Change the style of the header links.

--- src/components/headerLink.tsx
+++ src/components/headerLink.tsx
@@ -4,7 +4,7 @@ export function HeaderLink({ children, ...props }: HeaderLinkProps) {
   return (
     <div
       {...props}
-      className="select-none px-3 py-1 rounded-bl-2xl rounded-br-sm rounded-tl-sm rounded-tr-2xl hover:transition-colors backface-hidden group text-my-primary-text font-medium cursor-pointer flex items-center gap-1 hover:bg-my-text-blue"
+      className="select-none p-1 group text-my-primary-text font-medium cursor-pointer flex items-center gap-1 hover:opacity-70 transition-opacity active:opacity-50"
     >
       {children}
     </div>
