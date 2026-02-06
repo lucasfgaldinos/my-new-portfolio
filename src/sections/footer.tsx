a22387b8675f53b1bd2c0f72b29326c5f0e08a94
lucasfgaldinos <lucasfgaldinos@outlook.com>
2026-02-06 00:11:30 -0300

    style: Add a divider with a linear gradient in the footer.

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -2,9 +2,16 @@ export function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="border-t border-my-secondary-text">
-      <div className="max-w-5xl w-full py-4 md:py-10 px-4 md:px-10 mx-auto">
-        <p className="text-my-secondary-text text-center text-xs md:text-sm">
+    <footer>
+      <div className="max-w-5xl w-full py-6 md:py-10 px-4 md:px-10 mx-auto flex flex-col">
+        <div
+          className="max-w-sm mx-auto w-full h-px mb-6 md:mb-10"
+          style={{
+            background:
+              "linear-gradient(90deg, #0000, rgba(50,80,255, 0.40), #0000)",
+          }}
+        />
+        <p className="text-muted-foreground text-center text-xs md:text-sm">
           &copy; {currentYear} - <strong>Lucas Galdino</strong>. Todos os
           direitos reservados.
         </p>
